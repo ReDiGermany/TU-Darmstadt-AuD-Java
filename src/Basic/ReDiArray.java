7e3c6cd8bdcb3b16797f5c4ef4d0198f54164159
Max <support@redigermany.de>
2020-05-18 17:35:15 +0200

    Added debugger to ReDiArray.swap

--- src/Basic/ReDiArray.java
+++ src/Basic/ReDiArray.java
@@ -20,12 +20,29 @@ public class ReDiArray {
     public boolean equals(String inp){
         return Arrays.toString(A).equals(inp);
     }
+
+    private boolean log=false;
+    public void debug(){log=true;}
+
     /**
      * Method to swap two array elements
      * @param i the first element
      * @param j the second element
      */
     public void swap(int i, int j) {
+        if(log) {
+            System.out.println("Swap called!");
+            String[] B = new String[A.length];
+            Arrays.fill(B," ");
+            B[i]="=";
+            B[j]="=";
+            System.out.println("<" + Arrays.toString(A));
+            System.out.println(" " + Arrays.toString(B).
+                    replace(","," ").
+                    replace("["," ").
+                    replace("]"," "));
+        }
         A[i] = (A[i] + A[j]) - (A[j] = A[i]);
+        if(log) System.out.println(">"+Arrays.toString(A)+"\n");
     }
 }
